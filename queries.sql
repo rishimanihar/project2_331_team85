78663b5e512481503c3c84fbe7a4c6ae50f8338e
Rishi Manihar <rishimanihar@gmail.com>
2026-02-18 15:40:08 -0600

    added seocnd special query

--- queries.sql
+++ queries.sql
@@ -1,6 +1,19 @@
+/* Weekly Sales History
 SELECT
    EXTRACT(WEEK FROM order_time) AS week_number,
    COUNT(order_id) AS total_orders
 FROM Orders
 GROUP BY week_number
 ORDER BY week_number;
+*/
+
+
+/*Peak Sales Day*/
+SELECT
+   DATE(order_time) AS sale_day,
+   SUM(total_price) AS daily_revenue
+FROM Orders
+GROUP BY sale_day
+ORDER BY daily_revenue DESC
+LIMIT 10;
+
