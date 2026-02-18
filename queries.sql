fc143a9804ebe7ee5a07eadce6817d12492c12de
Rishi Manihar <rishimanihar@gmail.com>
2026-02-18 15:43:03 -0600

    added third speical query

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ ORDER BY week_number;
 */
 
 
-/*Peak Sales Day*/
+/*Peak Sales Day
 SELECT
    DATE(order_time) AS sale_day,
    SUM(total_price) AS daily_revenue
@@ -16,4 +16,14 @@ FROM Orders
 GROUP BY sale_day
 ORDER BY daily_revenue DESC
 LIMIT 10;
+*/
+
+/*Realistic Sales History*/
+SELECT
+   EXTRACT(HOUR FROM order_time) AS hour_of_day,
+   COUNT(order_id) AS num_orders,
+   SUM(total_price) AS hourly_revenue
+FROM Orders
+GROUP BY hour_of_day
+ORDER BY hour_of_day;
 
