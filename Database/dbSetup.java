90806fed595a8625f9f1a88df403c500e32ec9ad
Aayush Gadamshetty <aayushgadamshetty06@outlook.com>
2026-03-04 15:32:15 -0600

    changed login file

--- Database/dbSetup.java
+++ Database/dbSetup.java
@@ -1,9 +1,28 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 /*
 CSCE 331
 Database setup example
-Purpose: Keeping usernames and password private.
+Purpose: Loading credentials from a .env file to keep them private.
 */
-public final class dbSetup  {
-  public static final String user = "team_85";
-  public static final String pswd = "eighty_five";
-}//end class
+public final class dbSetup {
+    public String user;
+    public String pswd;
+
+    public dbSetup() {
+        Properties props = new Properties();
+        try {
+            // Looks for the .env file in the current directory
+            props.load(new FileInputStream("login.env"));
+            
+            // Assigns the values to the variables your other files use
+            this.user = props.getProperty("DB_USER");
+            this.pswd = props.getProperty("DB_PASS");
+            
+        } catch (Exception e) {
+            System.out.println("[ERROR] Could not load .env file");
+            e.printStackTrace();
+        }
+    }
+}
